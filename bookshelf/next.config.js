d28a351aeace1805d6d852dca36a6392b89bffb2
Rosh <roshanibankar11@gmail.com>
2026-08-21 20:34:06 +0530

    damn copilot

--- bookshelf/next.config.js
+++ bookshelf/next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/bookshelf",
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "**.public.blob.vercel-storage.com" },
